9e0435396bc9c39faf275ca6b6331e97bfa44c7e
AREPU PAVAN KUMAR <iosdeveloper.ipa@gmail.com>
2024-04-30 09:35:20 +0200

    Update store.js

--- redux/store.js
+++ redux/store.js
@@ -13,4 +13,4 @@ export const store = configureStore({
   },
 });
 
-export const server = "https://ecommerce-server-i8ns.onrender.com/api/v1";
+export const server = "https://ecommerce-server-firebaseURL-MongoDBURL/api/v1";
